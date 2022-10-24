8f04c96891df203c747c3257c4ae034ec1ebbf68
Züleyha Kurt <netice2017@gmail.com>
2022-10-24 11:40:13 +0300

    edit

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import mailSvg from "./assets/mail.svg";
 import manSvg from "./assets/man.svg";
 import womanSvg from "./assets/woman.svg";
@@ -9,11 +10,102 @@ import phoneSvg from "./assets/phone.svg";
 import padlockSvg from "./assets/padlock.svg";
 import cwSvg from "./assets/cw.svg";
 import Footer from "./components/footer/Footer";
+import axios from "axios";
 
-const url = "https://randomuser.me/api/";
-const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+
 
 function App() {
+
+  const [info, setInfo] = useState("")
+  const [man, setMan] = useState(false)
+  const [start, setStart] = useState(false)
+  const [click, setClick] = useState("")
+  const[tag,setTag]=useState("")
+
+  const url = "https://randomuser.me/api/";
+  const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+  
+  const getUser = async () => {
+    try {
+      const { data: { results } } = await axios(url)
+      console.log(results[0])
+                  const {
+                picture: {large},
+                name: {title, first, last},
+                email,
+                cell,gender,
+                location: {state, country},
+                    dob: { date, age },
+                login:{password}
+                  } = results[0]
+            
+              setInfo({
+                large,
+                title,
+                first, last,
+                email,cell,state,country,date,age,gender,password,
+              })
+      
+                if (gender === "female") {
+                setMan(true)
+                  }
+                else if (gender === "male") {
+                  setMan(false)
+                }
+              
+      setStart(true);
+      setTag("name");
+      setClick(info?.first)
+      
+  
+      
+      
+  
+  }
+    catch (error) {
+      console.log(error)
+    }
+   
+  }
+  
+  useEffect(() => {
+    getUser()
+  },[])
+
+ 
+  const getInfo = (e) => {
+    console.log(e.target.alt)
+    if (e.target.alt === "user") {
+      setClick(info.first+" "+info.last)
+      setTag("name")
+    }
+    if (e.target.alt === "mail") {
+      setClick(info.email)
+      setTag("e-mail")
+    }
+    if (e.target.alt === "age") {
+      setClick(info.age)
+      setTag("age")
+    }
+    if (e.target.alt === "map") {
+      setClick(info.state+"/"+info.country)
+      setTag("adress")
+    }
+    if (e.target.alt === "phone") {
+      setClick(info.cell)
+      setTag("phone")
+    }
+    if (e.target.alt === "lock") {
+      setClick(info.password)
+      setTag("password")
+    }
+
+
+}
+ 
+
+
+
   return (
     <main>
       <div className="block bcg-orange">
@@ -21,34 +113,34 @@ function App() {
       </div>
       <div className="block">
         <div className="container">
-          <img src={defaultImage} alt="random user" className="user-img" />
-          <p className="user-title">My ... is</p>
-          <p className="user-value"></p>
+          <img src={start? info?.large : defaultImage } alt="random user" className="user-img" />
+          <p className="user-title">My {tag} is</p>
+          <p className="user-value">{click}</p>
           <div className="values-list">
             <button className="icon" data-label="name">
-              <img src={womanSvg} alt="user" id="iconImg" />
+              <img src={man? womanSvg : manSvg} alt="user" id="iconImg" onClick={getInfo}  />
             </button>
             <button className="icon" data-label="email">
-              <img src={mailSvg} alt="mail" id="iconImg" />
+              <img src={mailSvg} alt="mail" id="iconImg" onClick={getInfo}/>
             </button>
             <button className="icon" data-label="age">
-              <img src={womanAgeSvg} alt="age" id="iconImg" />
+              <img src={man? womanAgeSvg : manAgeSvg} alt="age" id="iconImg" onClick={getInfo} />
             </button>
             <button className="icon" data-label="street">
-              <img src={mapSvg} alt="map" id="iconImg" />
+              <img src={mapSvg} alt="map" id="iconImg" onClick={getInfo} />
             </button>
             <button className="icon" data-label="phone">
-              <img src={phoneSvg} alt="phone" id="iconImg" />
+              <img src={phoneSvg} alt="phone" id="iconImg" onClick={getInfo} />
             </button>
             <button className="icon" data-label="password">
-              <img src={padlockSvg} alt="lock" id="iconImg" />
+              <img src={padlockSvg} alt="lock" id="iconImg"  onClick={getInfo}/>
             </button>
           </div>
           <div className="btn-group">
-            <button className="btn" type="button">
+            <button className="btn" type="button" onClick={()=>getUser()}>
               new user
             </button>
-            <button className="btn" type="button">
+            <button className="btn" type="button" o>
               add user
             </button>
           </div>
@@ -72,7 +164,6 @@ function App() {
         <Footer />
       </div>
     </main>
-  );
+)
 }
-
 export default App;
